e57c7015946f6c81b56e4f09cc21ac56afbdeb0d
hariom7492 <hariom7492@gmail.com>
2020-06-05 19:16:04 +0530

    Updates

---
+++ precache-manifest.1a3bd539c678c8dd61b44ab737702f09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ce185cbe5c759a8b488747243b1bc2",
+    "url": "/react-frinds-cards/index.html"
+  },
+  {
+    "revision": "2e3b4d52c210f3b288a0",
+    "url": "/react-frinds-cards/static/css/main.0fec849e.chunk.css"
+  },
+  {
+    "revision": "45e4956f080a66c14e4e",
+    "url": "/react-frinds-cards/static/js/2.75d852b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-frinds-cards/static/js/2.75d852b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e3b4d52c210f3b288a0",
+    "url": "/react-frinds-cards/static/js/main.b934b000.chunk.js"
+  },
+  {
+    "revision": "44d3f872a9b891e70ce4",
+    "url": "/react-frinds-cards/static/js/runtime-main.31d6342e.js"
+  }
+]);
